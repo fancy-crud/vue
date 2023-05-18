d69c5314a1b6362294e22c88e0f9c265d23e6ef2
Christopher A. Flores <christopher.flores@codexitos.com>
2023-05-17 21:58:49 -0600

    chore: remove legacy code

--- src/settings/composables/index.ts
+++ src/settings/composables/index.ts
@@ -1,8 +1,5 @@
 import type { HandleRequestStatusCodes } from '@/http'
 
-import FInputDate from '@/inputs/components/FInputDate.vue'
-import FInputFile from '@/inputs/components/FInputFile.vue'
-
 const defaults: { classes: Record<string, string> } = {
   classes: {
     // text: 'text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
