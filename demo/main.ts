d860e1f0a4ba46cbeae34b4800429c90ae253358
Christopher A. Flores <christopher.flores@codexitos.com>
2023-05-21 21:42:57 -0600

    refactor: remove legacy modules

--- demo/main.ts
+++ demo/main.ts
@@ -1,22 +1,20 @@
 import { createApp } from 'vue'
-import axios from 'axios'
 import '@oruga-ui/oruga-next/dist/oruga-full.css'
 // import Oruga from '@oruga-ui/oruga-next'
 import FancyCrud from '../src/index'
 import { buttons, fields } from '../src/wrappers/config'
+import _axios from './modules/axios'
 import App from './App.vue'
 
 import '@mdi/font/css/materialdesignicons.css'
 // import 'flowbite'
 import './styles/main.sass'
 
-axios.defaults.baseURL = 'http://localhost:9000/api/'
-
 const app = createApp(App)
 
 // app.use(Oruga)
 app.use(FancyCrud, {
-  http: { axios },
+  http: { axios: _axios },
   controls: {
     ...fields,
   },
