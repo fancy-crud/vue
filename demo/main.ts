33f1c3fad10399bd482795f94d4156d7326640c5
Christopher A. Flores <christopher.flores@codexitos.com>
2023-05-21 21:46:48 -0600

    chore: use default axios instance

--- demo/main.ts
+++ demo/main.ts
@@ -1,20 +1,22 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import '@oruga-ui/oruga-next/dist/oruga-full.css'
 // import Oruga from '@oruga-ui/oruga-next'
 import FancyCrud from '../src/index'
 import { buttons, fields } from '../src/wrappers/config'
-import _axios from './modules/axios'
 import App from './App.vue'
 
 import '@mdi/font/css/materialdesignicons.css'
 // import 'flowbite'
 import './styles/main.sass'
 
+axios.defaults.baseURL = 'http://localhost:9000/api/'
+
 const app = createApp(App)
 
 // app.use(Oruga)
 app.use(FancyCrud, {
-  http: { axios: _axios },
+  http: { axios },
   controls: {
     ...fields,
   },
