a8374d8b9adfafb378531afdcb3833206a65d69e
Christopher A. Flores <christopher.flores@codexitos.com>
2023-05-29 20:38:35 -0600

    fix: remove axioma + capabilities for http module

--- src/http/axioma/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './services'
-export * from './typing'
-export * from './value-objects'
--- src/http/capabilities/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './common'
-export * from './request-create'
-export * from './request-retrieve'
-export * from './request-delete'
-export * from './request-list'
-export * from './request-update'
